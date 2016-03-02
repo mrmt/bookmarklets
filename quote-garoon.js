1f876083acab2a32105d9cd9b77068b0d162d5a6
Jun Morimoto <morimoto@mrmt.net>
2016-03-02 12:03:14 +0900

    施設が無い予定でも動作するようfix

--- quote-garoon.js
+++ quote-garoon.js
@@ -2,11 +2,15 @@ javascript:
 (
     function(d)
     {
-	title = d.querySelector('h2[class="schedule"]').innerText;
+	var param = [];
+	param.push(title = d.querySelector('h2[class="schedule"]').innerText);
 	date = d.querySelector('div[class="groupDayCalendarDate-grn"]').innerText.replace('）', ')').replace('（', '(');
 	time = d.querySelector('span[class="mLeft15"]').innerText.replace(/ /g, '');
-	url = 'https://' + d.location.hostname + d.location.pathname + '?event=' + d.querySelector('input[name="event"]').value;
-	room = d.querySelector('span[class="facility-grn"]').innerText;
-	prompt('link', [title, date+' '+time, room, url].join("\n"));
+	param.push(date + " " + time);
+	param.push('https://' + d.location.hostname + d.location.pathname + '?event=' + d.querySelector('input[name="event"]').value);
+	if((room = d.querySelector('span[class="facility-grn"]')) != null){
+	    param.push(room.innerText);
+	}
+	prompt('link', param.join("\n"));
     }
 )(document);
