5e7c3c82c2abf7d3a657261567a0e51c22a171a3
Jun Morimoto <morimoto@mrmt.net>
2016-04-01 10:20:42 +0900

    日時と場所を同一行にまとめた

--- quote-garoon.js
+++ quote-garoon.js
@@ -4,13 +4,13 @@ javascript:
     {
 	var param = [];
 	param.push(title = d.querySelector('h2[class="schedule"]').innerText);
-	date = d.querySelector('div[class="groupDayCalendarDate-grn"]').innerText.replace('）', ')').replace('（', '(');
-	time = d.querySelector('span[class="mLeft15"]').innerText.replace(/ /g, '');
-	param.push(date + " " + time);
-	param.push('https://' + d.location.hostname + d.location.pathname + '?event=' + d.querySelector('input[name="event"]').value);
+	spec = d.querySelector('div[class="groupDayCalendarDate-grn"]').innerText.replace('）', ')').replace('（', '(');
+	spec = " " + d.querySelector('span[class="mLeft15"]').innerText.replace(/ /g, '');
 	if((room = d.querySelector('span[class="facility-grn"]')) != null){
-	    param.push(room.innerText);
+		spec = spec + " " + room.innerText;
 	}
+	param.push(spec);
+	param.push('https://' + d.location.hostname + d.location.pathname + '?event=' + d.querySelector('input[name="event"]').value);
 	prompt('link', param.join("\n"));
     }
 )(document);
