a6ac2822e2ef79e4241272d111e9609397d356ad
Jun Morimoto <morimoto@mrmt.net>
2016-05-10 01:09:21 +0900

    2016.05.08版cybozu.comに追従

--- quote-garoon.js
+++ quote-garoon.js
@@ -3,14 +3,16 @@ javascript:
     function(d)
     {
 	var param = [];
-	param.push(title = d.querySelector('h2[class="schedule"]').innerText);
-	spec = d.querySelector('div[class="groupDayCalendarDate-grn"]').innerText.replace('）', ')').replace('（', '(');
-	spec = " " + d.querySelector('span[class="mLeft15"]').innerText.replace(/ /g, '');
+	title = d.querySelector('h2[class="schedule"]').innerText;
+	param.push(title);
+	date = d.querySelector('span[class="schedule_text_noticeable_grn"]').innerText;
+	param.push(date);
 	if((room = d.querySelector('span[class="facility-grn"]')) != null){
-		spec = spec + " " + room.innerText;
+	    room = room.innerText;
+	    param.push(room);
 	}
-	param.push(spec);
-	param.push('https://' + d.location.hostname + d.location.pathname + '?event=' + d.querySelector('input[name="event"]').value);
-	prompt('link', param.join("\n"));
+	url = 'https://' + d.location.hostname + d.location.pathname + '?event=' + d.querySelector('input[name="event"]').value;
+	param.push(url);
+	prompt('ガルーン予定の要約', param.join("\n").replace(/）/g, ')').replace(/（/g, '(').replace(/ /g, ''));
     }
 )(document);
