9414ab5991836620a24a5e96fd8a2326b0b51c99
Jun Morimoto <morimoto@mrmt.net>
2017-10-11 12:30:01 +0900

    backlogにも対応。urlのhash fragmentを除去。

--- quote-jira.js
+++ quote-jira.js
@@ -2,6 +2,6 @@ javascript:
 (
     function(d)
     {
-	prompt('', d.title.replace('[','').replace(']','').replace(' - Mixi Jira','\n'+d.location.href));
+	prompt('', d.title.replace('[','').replace(']','').replace(' - Mixi Jira','').replace(' | 課題の表示 - Backlog', '') + '\n' + d.location.href.replace(/#.*/, ''));
     }
 )(document);
