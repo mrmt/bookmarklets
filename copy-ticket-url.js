6b34bfb7d60150c80083363f1584be1013e2f7e7
Jun Morimoto <morimoto@mrmt.net>
2021-03-04 12:09:51 +0900

    fix minor regexp for Backlog

--- copy-ticket-url.js
+++ copy-ticket-url.js
@@ -15,7 +15,7 @@ global.COPY_TO_CLIPBOARD.getContent = function(d){
 	"\\]" : "",
 	" - Jira" : "",
 	" - Mixi Jira" : "",
-	" \\| 課題の表示 - Backlog" : "",
+	" \\| 課題の表示 \\| Backlog" : "",
 	'^(.*) · Issue #(\\d\\d).*' : '#$2 $1'
     }) + "\n" + d.URL.replace(/#.*/, '') + "\n";
 };
