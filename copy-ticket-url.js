75fffd88e911fe6a1cbdc61de50bfc43467f552e
Jun Morimoto <morimoto@mrmt.net>
2022-03-09 10:26:49 +0900

    fix number glob of github issue

--- copy-ticket-url.js
+++ copy-ticket-url.js
@@ -16,7 +16,7 @@ global.COPY_TO_CLIPBOARD.getContent = function(d){
 	" - Jira" : "",
 	" - Mixi Jira" : "",
 	" . 課題の表示 . Backlog" : "",
-	'^(.*) · Issue #(\\d\\d).*' : '#$2 $1'
+	'^(.*) · Issue #(\\d+).*' : '#$2 $1'
     }) + "\n" + d.URL.replace(/#.*/, '') + "\n";
 };
 
