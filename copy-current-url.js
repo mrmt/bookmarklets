d1d060551b49f2d21b44dfcb413fe5fd1117d110
Jun Morimoto <morimoto@gmail.com>
2022-03-10 09:54:06 +0900

    merge copy-ticket-url

--- copy-current-url.js
+++ copy-current-url.js
@@ -2,19 +2,6 @@ javascript:
 var global = window;
 global.COPY_TO_CLIPBOARD = global.COPY_TO_CLIPBOARD || {};
 global.COPY_TO_CLIPBOARD.getContent = function(d){
-    let u = d.URL;
-    let s;
-    if(s = d.querySelector('link[rel="shortlink"]')){
-        u = s.href;
-    };
-    if(s = d.querySelector('link[rel="canonical"]')){
-        u = s.href;
-    };
-    u = u.replace(/#gid=\d+$/, '');
-    u = u.replace(/#slide=id\.[a-z0-9_]+$/, '');
-    u = u.replace(/#heading=h\.[a-z0-9_]+$/, '');
-    u = u.replace(/fbclid=[a-zA-Z0-9-]*/, '');
-
     let t = new String(d.title);
     t.allReplace = function(t){
         let b = this,c;
@@ -23,12 +10,26 @@ global.COPY_TO_CLIPBOARD.getContent = function(d){
         }
         return b;
     }.bind(t);
+
+    let u = d.URL, s;
+    if(s = d.querySelector('link[rel="shortlink"]')){ u = s.href; }
+    if(s = d.querySelector('link[rel="canonical"]')){ u = s.href; }
+    u = u.replace(/#gid=\d+$/, '');
+    u = u.replace(/#slide=id\.[a-z0-9_]+$/, '');
+    u = u.replace(/#heading=h\.[a-z0-9_]+$/, '');
+    u = u.replace(/fbclid=[a-zA-Z0-9-]*/, '');
+
     return t.allReplace({
         " - Confluence" : "",
         " - Google スプレッドシート" : "",
         " - Google スライド" : "",
         " - Google ドライブ" : "",
         " - Google ドキュメント" : "",
+        " - Jira" : "",
+        " - Mixi Jira" : "",
+        "\\[(\\w+-\\d+)\\]" : "$1",
+        " . 課題の表示 . Backlog" : "",
+        '^(.*) · Issue #(\\d+).*' : '#$2 $1'
     }) + "\n" + u + "\n";
 };
 
