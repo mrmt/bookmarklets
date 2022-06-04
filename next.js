bc6da61fff0c92dc222372b74fb35b2cfb5e9b41
Jun Morimoto <morimoto@gmail.com>
2022-06-05 00:05:29 +0900

    add

---
+++ next.js
@@ -0,0 +1,8 @@
+javascript:
+(
+    function(loc, path){
+	if(matched = path.match(/^(.*)\/([0-9]+)$/)){
+	    loc.href = `${matched[1]}/${Number(matched[2])+1}`;
+	}
+    }
+)(location, location.pathname);
